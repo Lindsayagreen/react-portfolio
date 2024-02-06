2e36de491f7b70d3cef01b94582d56156e75da72
lindsayagreen <lindsgree@gmail.com>
2024-02-06 09:15:14 -0600

    init

---
+++ components/PortfolioContainer.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import Portfolio from './pages/Portfolio';
+import AboutMe from './pages/About';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Nav from '/NavTabs';
+
