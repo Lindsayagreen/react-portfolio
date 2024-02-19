b1440a0ef4331fd1674208fda8a3064c428c1d83
lindsayagreen <lindsgree@gmail.com>
2024-02-19 13:36:05 -0600

    added functinality to pages

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -1,24 +1,56 @@
+// import "../../styles/AboutMe.css";
+// import React from "react";
+
+// export default function Aboutme() {
+//   return (
+//     <div className="card">
+//       <img src="..." className="card-img-top" alt="..." />
+//       <div className="card-body">
+//         <p className="card-text">
+          
+//         </p>
+
+//         <div className="headerName">
+//           <p> Hi, I'm Lindsay, a passionate and creative leader, learner, and trainer based in DFW. I have a strong foundation in management, coaching, and elevating and I am constantly seeking opportunities to further develop my skills and expertise. </p>
+//         </div>
+//         <div className="aboutMeQuote">
+//           <p> to add to my skills,I completed a full-stack web development program with Southern MEthodist University. I received my bachelors in political scvience from the University of Oklahoma</p>
+//         </div>
+//         <div className="aboutmeSection">
+//           <div className="paragraph">
+//             <p>My previous experience is in retail and food and beverage management. </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import "../../styles/AboutMe.css";
 import React from "react";
 
 export default function Aboutme() {
   return (
-    <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <p className="card-text">
-          
-        </p>
-
-        <div className="headerName">
-          <p> Hi, I'm Lindsay, a passionate and creative leader, learner, and trainer based in DFW. I have a strong foundation in management, coaching, and elevating and I am constantly seeking opportunities to further develop my skills and expertise. </p>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <img src="/my-image.jpg" className="img-fluid" alt="Profile" />
         </div>
-        <div className="aboutMeQuote">
-          <p> to add to my skills,I completed a full-stack web development program with Southern MEthodist University. I received my bachelors in political scvience from the University of Oklahoma</p>
-        </div>
-        <div className="aboutmeSection">
-          <div className="paragraph">
-            <p>My previous experience is in retail and food and beverage management. </p>
+        <div className="col-md-9">
+          <div className="card">
+            <div className="card-body">
+              <div className="headerName">
+                <p> Hi, I'm Lindsay, a passionate and creative leader, learner, and trainer based in DFW. I have a strong foundation in management, coaching, and elevating and I am constantly seeking opportunities to further develop my skills and expertise. </p>
+              </div>
+              <div className="aboutMeQuote">
+                <p> to add to my skills,I completed a full-stack web development program with Southern MEthodist University. I received my bachelors in political scvience from the University of Oklahoma</p>
+              </div>
+              <div className="aboutmeSection">
+                <div className="paragraph">
+                  <p>My previous experience is in retail and food and beverage management. </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -3,58 +3,70 @@ import React from "react";
 import "../../styles/Portfolio.css";
 
 const Portfolio = () => {
-    const projects = [
-        {
-            name: 'Run Buddy',  
-            description: 'A website that offers fitness training services.',
-            link: 'link here',
-            github: 'github link here',
-            image: 'img here',
-        },
-        {
-            name: 'Run Buddy',  
-            description: 'A website that offers fitness training services.',
-            link: 'link here',
-            github: 'github link here',
-            image: 'img here',
-        },
-        {
-            name: 'Run Buddy',  
-            description: 'A website that offers fitness training services.',
-            link: 'link here',
-            github: 'github link here',
-            image: 'img here',
-        },
-        {
-            name: 'Run Buddy',  
-            description: 'A website that offers fitness training services.',
-            link: 'link here',
-            github: 'github link here',
-            image: 'img here',
-        },
-    ]
-    return (
-        <div className="portfolio-container">
-          <h1>Portfolio</h1>
-          <p>fkgjhksdfjgksdfj</p>
-          {projects.map((project) => (
-            <div className="card" key={project.name}>
-              <img src={project.image} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{project.name}</h5>
-                <p className="card-text">{project.description}</p>
-                <a href={project.link} className="btn btn-primary">
-                  demo app
-                </a>
-                <a href={project.github} className="btn btn-primary">
-                  github
-                </a>
-              </div>
-            </div>
-          ))}
+  const projects = [
+    {
+      name: "Employee Tracker",
+      description: "Using sql, a webpage that tracks employees, their roles and departments.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/sql-emp-tracker.git",
+      image: "img here",
+    },
+    {
+      name: "Readme Generator",
+      description: "A wepage rthat generates a readme file for a project.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/professional-README-generator.git",
+      image: "img here",
+    },
+    {
+      name: "Weather Dashboard",
+      description: "A website that offers fitness training services.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/professional-README-generator.git",
+      image: "img here",
+    },
+    {
+      name: "Portfolio using React",
+      description: "A porfolio webpage using react.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/react-portfolio.git",
+      image: "img here",
+    },
+    {
+      name: "Texas Sports Blog",
+      description: "A social media blog for users to add their posts about Texas sports.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/sportsblog-p2.git",
+      image: "img here",
+    },
+    {
+      name: "Backend Logo Generator",
+      description: "A backend logo generator.",
+      link: "link here",
+      github: "https://github.com/Lindsayagreen/svg-logo.git",
+      image: "img here",
+    },
+  ];
+  return (
+    <div className="portfolio-container">
+      <h1>Portfolio</h1>
+      {projects.map((project) => (
+        <div className="card" key={project.name}>
+          <img src={project.image} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{project.name}</h5>
+            <p className="card-text">{project.description}</p>
+            {/* <a href={project.link} className="btn btn-primary">
+              demo app
+            </a> */}
+            <a href={project.github} className="btn btn-primary">
+              github
+            </a>
+          </div>
         </div>
-      );
-    };
+      ))}
+    </div>
+  );
+};
 
 export default Portfolio;
-
